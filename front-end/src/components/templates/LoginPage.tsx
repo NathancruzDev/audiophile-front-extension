211f2a64d5e7ae381478f7e22d99ccc6a42a919a
nathan-ubuntu-pc <nathanleitedacruz@gmail.com>
2026-02-22 17:06:25 -0300

    addd

---
+++ front-end/src/components/templates/LoginPage.tsx
@@ -0,0 +1,16 @@
+import { Container } from '@chakra-ui/react'
+
+import Hero from 'components/organisms/Hero'
+import CategoriesLinks from 'components/organisms/CategoriesLinks'
+import MainGallery from 'components/organisms/MainGallery'
+import AnimatedBestGear from 'components/molecules/AnimatedBestGear'
+
+
+const LoginPage=():JSX.Element => {
+    return(
+        <h1>LoginPage</h1>
+    )
+}
+
+
+export default LoginPage
